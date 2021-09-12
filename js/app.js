711d0c55ef5c17c4f424b4a8970c6b85182559e7
mdtanvir037 <hossainmdjabed037@gmail.com>
2021-09-13 03:55:52 +0600

    after new api

--- js/app.js
+++ js/app.js
@@ -14,24 +14,65 @@ const loadProducts = () => {
 };
 loadProducts();
 
+// show product details in UI
+const showDeatils = id => {
+  fetch(`https://fakestoreapi.com/products/${id}`)
+            .then(res=>res.json())
+            .then(data=>showDeatilsUi(data))
+  
+}
+/* const showDeatilsUi = detail => {
+    const img = detail.image;
+    const titles = detail.title.slice(0, 20)
+  const description = detail.description.slice(0, 200)
+  const details = document.querySelector('.details-on-ui')
+  details.textContent = ''
+    const div = document.createElement('div')
+    div.classList.add('mx-auto')
+    div.innerHTML = `
+    <div class="col">
+     <div class="card">
+      <img src="${img}" class="card-img-top show-details-img" alt="Releted image">
+      <div class="card-body">
+        <h2 class="card-title fw-bold">${titles}</h2>
+        <h2 class="card-text mb-0 fw-bold"> $ ${detail.price}</h2>
+        <p class="card-text mb-2 fw-bold">${detail.category}</p>
+        <p class="font-bold mb-0 d-flex justify-content-start">
+        <span class="me-3"><i class="fas fa-grin-stars"></i> ${detail.rating.rate}</span>   <span>${detail.rating.count} <i class="fas fa-user"></i> </span>
+      </p>
+        <p class="card-text mt-3 text-muted">${description}</p>
+      </div>
+    </div>
+  </div>
+  
+  `
+  details.appendChild(div)
+  }
+// show product details in UI  */
+
 // show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
-  console.log(allProducts)
   for (const product of allProducts) {
     const image = product.image;
-    console.log(image)
+    const title = product.title.slice(0, 20)
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.classList.add("h-100");
+    div.innerHTML = `<div class="single-product h-100">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h3>${title}</h3>
       <p>Category: ${product.category}</p>
+      <p class="font-bold d-flex justify-content-evenly">
+      <span><i class="fas fa-grin-stars"></i> ${product.rating.rate}</span> 
+      <span>${product.rating.count} <i class="fas fa-user"></i> </span>
+      </p>
+
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-secondary">add to cart</button>
+      <button id="details-btn" onclick="showDeatils('${product.id}')" class="btn btn-dark">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
